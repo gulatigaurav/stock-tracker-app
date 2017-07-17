f1da0016ccf3b4127b69a5c3c8a43f55f396f4d0
Gaurav Gulati <gauravgulati9c@gmail.com>
2017-07-17 16:55:39 +0530

    User Name added to Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
- devise_for :users
- resources :user_stocks, except: [:edit, :update, :show]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'welcome#index'
-get 'welcome' ,to: 'welcome#index'
-get 'myportfolio', to: 'users#my_portfolio'
 
-get 'search_stocks', to: 'stocks#search'
+        devise_for :users, :controllers => {:registrations => 'user/registrations'}
+
+    resources :user_stocks, except: [:edit, :update, :show]
+      # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    root 'welcome#index'
+    get 'welcome' ,to: 'welcome#index'
+    get 'myportfolio', to: 'users#my_portfolio'
+
+    get 'search_stocks', to: 'stocks#search'
 
 
 end
